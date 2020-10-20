71672d63e6f6535972c687cea62d5fe0d245227c
P. Anuraag Reddy <anuraagreddy74@outlook.com>
2020-10-20 17:37:08 +0530

    Parking_Spot class is present in here

---
+++ Parking_Spot.java
@@ -0,0 +1,26 @@
+class Parking_Spot {
+    //Variables of class
+    private String typeOfSpot;
+    private boolean isEmpty;
+    private boolean isElectrical;
+    private String typeOfVehicle;
+
+    //Constructor
+    Parking_Spot(String typeOfSpot) {
+        this.typeOfSpot = typeOfSpot;
+        this.isEmpty = true;
+    }
+
+    //Methods (Change name of methods if better are possible)
+    //To set the isEmpty false when customer comes
+    public void setParked(Customer c) {             //Assuming based on customer object
+        //Set typeOfVehicle using customer's variable like "c.typeOfVehicle"
+        this.isEmpty = false;
+    }
+
+    //To get the status of parking
+    public Boolean getStatusParked() {
+        return this.isEmpty;
+    }
+}
+
